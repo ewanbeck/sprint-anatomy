cffbdd0b5cc9f649707c11ff54f9f534d22e3ccb
Ewan Beckett <ewan.beckett@gmail.com>
2023-02-07 02:28:52 +0100

    Updated vite-plugin-favicon options to add some info fields to manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,17 @@ import { ViteFaviconsPlugin } from "vite-plugin-favicon2";
 export default defineConfig({
   plugins: [
     react(),
-    ViteFaviconsPlugin({logo: "src/assets/sprint_anatomy_favicon.svg"}),
+    ViteFaviconsPlugin({
+      logo: "src/assets/sprint_anatomy_favicon.svg",
+      favicons: {
+        appName: "Anatomy of a sprint",
+        appShortName: "Sprint Anatomy",
+        appDescription:
+          "A tiny react UI demo showing how a 2 week development sprint might look.",
+        developerName: "Ewan Beckett",
+        developerURL: "https://github.com/ewanbeck"
+      },
+    }),
   ],
-  base: '/sprint-anatomy/'
+  base: "/sprint-anatomy/",
 });
